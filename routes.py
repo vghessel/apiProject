87b26620eb3e4d74ea39931988beff1ec5c84270
Vinicius Gerlach Hessel <vinighessel@outlook.com>
2022-04-26 14:04:11 -0300

    POST

--- routes.py
+++ routes.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 import sqlite3
 import xml.etree.ElementTree as ET
 
-
 app = Flask("main")
 
+
+#GET
 
 @app.route("/employees", methods=["GET"])   
 def companyEmployees():
@@ -31,8 +32,9 @@ def companyEmployees():
     return jsonify(retorno)
 
 
+#GET INDIVIDUAL
 
-@app.route("/employees/id")    #verificar como colocar o id no endpoint
+@app.route("/employees/id")    
 def companyEmployee():
 
     body = request.get_json()
@@ -47,6 +49,60 @@ def companyEmployee():
     result = cur.execute("SELECT * from employees WHERE id = '{0}';".format(body["id"]))
 
     return jsonify(result.fetchall())
+
+
+#POST
+
+@app.route("/employees/add", methods=["POST"])
+def addEmployee():
+
+    body = request.get_json()
+
+    if("id" not in body):
+        return generateResponse(400, "The id parameter is required!")
+    if ("name" not in body):
+        return generateResponse(400, "The name parameter is required!")
+    if ("position" not in body):
+        return generateResponse(400, "The position parameter is required!")
+    if ("countrycode" not in body):
+        return generateResponse(400, "The countrycode parameter is required!")
+
+    tree = ET.parse('dbpath.xml')
+    root = tree.getroot()
+    adress = tree.find('dbpath').text
+
+    conn = sqlite3.connect(adress)
+    cur = conn.cursor()
+
+    result = cur.execute('''
+    INSERT INTO 
+        employees (id, name, position, countrycode) 
+    VALUES ('{0}', '{1}', '{2}', '{3}');
+    '''.format(body["id"], body["name"], body["position"], body["countrycode"]))
+
+    conn.commit()
+    conn.close()
+
+    return generateResponse(200, "Added employee!", "name:", body["name"])
+
+
+#PUT
+
+
+#DELETE
+
+
+#RESPONSES
+
+def generateResponse(status, message, content_name=False, content=False):
+    response = {}
+    response["status"] = status
+    response["message"] = message
+
+    if(content_name and content):
+        response[content_name] = content
+
+    return response
 
 
 app.run()
