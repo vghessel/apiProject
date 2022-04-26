5ddbc6a6fe3ee77f6e6d634745ae23e9f3358ca4
Vinicius Gerlach Hessel <vinighessel@outlook.com>
2022-04-26 11:59:45 -0300

    GET

---
+++ routes.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify
+import sqlite3
+import xml.etree.ElementTree as ET
+
+
+app = Flask("main")
+
+
+@app.route("/employees", methods=["GET"])   
+def companyEmployees():
+
+    tree = ET.parse('dbpath.xml')
+    root = tree.getroot()
+    adress = tree.find('dbpath').text
+
+    conn = sqlite3.connect(adress)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    result = cur.execute("SELECT * from employees;")
+    
+    retorno = []
+    for row in result.fetchall():
+        item = {}
+        item['id'] = row['id']
+        item['name'] = row['name']
+        item['position'] = row['position']
+        item['countrycode'] = row['countrycode']
+        retorno.append(item)
+
+    return jsonify(retorno)
+
+
+
+@app.route("/employees/id")    #verificar como colocar o id no endpoint
+def companyEmployee():
+
+    body = request.get_json()
+
+    tree = ET.parse('dbpath.xml')
+    root = tree.getroot()
+    adress = tree.find('dbpath').text
+
+    conn = sqlite3.connect(adress)
+    cur = conn.cursor()
+
+    result = cur.execute("SELECT * from employees WHERE id = '{0}';".format(body["id"]))
+
+    return jsonify(result.fetchall())
+
+
+app.run()
